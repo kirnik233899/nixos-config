ca2b64e2f37efebe45c2a875cd171b1e7f82c32e
Kirnik <268614269+kirnik233899@users.noreply.github.com>
2026-06-15 00:56:32 +0300

    Update configuration.nix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -33,3 +33,6 @@
 
   services.logind.settings.Login.HandleLidSwitch = "suspend";
 }
+
+environment.sessionVariables.__EGL_VENDOR_LIBRARY_FILENAMES =
+    "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
