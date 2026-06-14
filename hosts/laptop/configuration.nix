28fc236d125b1c9c636bdd518ccb9a2f5d81fb65
Kirnik <268614269+kirnik233899@users.noreply.github.com>
2026-06-14 22:45:51 +0300

    Create configuration.nix

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/common.nix
+  ];
+
+  networking.hostName = "laptop";
+
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+  ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:2:0:0";
+    };
+  };
+
+  services.power-profiles-daemon.enable = true;
+  services.thermald.enable = true;
+  services.upower.enable = true;
+
+  services.logind.settings.Login.HandleLidSwitch = "suspend";
+}
